70c2baa1655677fa7469be00bb0066315111bf4b
Prerit Khandelwal <127297823+GeegPrerit@users.noreply.github.com>
2023-05-28 13:39:29 +0530

    Update CountAndFilter.cs

--- Controllers/CountAndFilter.cs
+++ Controllers/CountAndFilter.cs
@@ -1,37 +1,100 @@
+using Hotel_Management.Auth;
+using Hotel_Management.Exceptions;
+using Hotel_Management.Model;
 using Hotel_Management.Repository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Hotel_Management.Controllers
 {
-   /* [Authorize]*/
+    //[Authorize(Roles = UserRoles.Owner)]
     [Route("api/[controller]")]
     [ApiController]
-    public class CountAndFilter : ControllerBase
+    public class HotelManagementController : ControllerBase
     {
-        private readonly ICountFilterRepository _hotelRepository;
+        private readonly IHotelRepository _hotelRepository;
 
-        public CountAndFilter(ICountFilterRepository hotelRepository)
+        public HotelManagementController(IHotelRepository hotelRepository)
         {
             _hotelRepository = hotelRepository;
         }
+
         [HttpGet]
-        //[Route("Filter")]
-        public IActionResult GetAllHotels(string location = null, decimal? minPrice = null, decimal? maxPrice = null)
+        public IActionResult GetAllHotels()
         {
-            var hotels = _hotelRepository.GetFilteredHotels(location, minPrice, maxPrice);
-            return Ok(hotels);
+            try
+            {
+                var hotels = _hotelRepository.GetAllHotels();
+                return Ok(hotels);
+            }
+            catch (AuthorizationException)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "You are not authorized to access this resource.");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while retrieving the list of hotels.");
+            }
         }
 
         [HttpGet("{id}")]
-        //[Route("Count-Available-Rooms")]
-        public int GetCount(int id)
+        public IActionResult GetHotelById(int id)
         {
-            var res = _hotelRepository.Results(id);
+            try
+            {
+                var hotel = _hotelRepository.GetHotelById(id);
+                if (hotel == null)
+                    return NotFound($"Hotel with ID {id} not found.");
 
-            return res;
+                return Ok(hotel);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while retrieving the hotel details.");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateHotel([FromBody] Hotel hotel)
+        {
+            try
+            {
+                _hotelRepository.AddHotel(hotel);
+                return CreatedAtAction(nameof(GetHotelById), new { id = hotel.Id }, hotel);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while creating the hotel.");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateHotel(int id, [FromBody] Hotel hotel)
+        {
+            try
+            {
+                _hotelRepository.UpdateHotel(id, hotel);
+                return Ok(hotel);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"An error occurred while updating the hotel with ID {id}.");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteHotel(int id)
+        {
+            try
+            {
+                _hotelRepository.DeleteHotel(id);
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"An error occurred while deleting the hotel with ID {id}.");
+            }
         }
     }
 }
